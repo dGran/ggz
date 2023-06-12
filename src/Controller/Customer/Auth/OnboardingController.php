371a453918506afafcba0db73c607c95afbd1da4
dgrandev <dgranh@gmail.com>
2023-06-13 00:35:31 +0200

    [IMP] Improved code

--- src/Controller/Customer/Auth/OnboardingController.php
+++ src/Controller/Customer/Auth/OnboardingController.php
@@ -11,18 +11,18 @@ class OnboardingController extends AbstractController
     #[Route('/onboarding/step-one', name: 'customer_onboarding_step_one')]
     public function stepOne(): Response
     {
-        return $this->render('customer/auth/onboarding/step_one.html.twig');
+        return $this->render('customer/auth/signup/onboarding/step_one.html.twig');
     }
 
     #[Route('/onboarding/step-two', name: 'customer_onboarding_step_two')]
     public function stepTwo(): Response
     {
-        return $this->render('customer/auth/onboarding/step_two.html.twig');
+        return $this->render('customer/auth/signup/onboarding/step_two.html.twig');
     }
 
     #[Route('/onboarding/step-three', name: 'customer_onboarding_step_three')]
     public function stepThree(): Response
     {
-        return $this->render('customer/auth/onboarding/step_three.html.twig');
+        return $this->render('customer/auth/signup/onboarding/step_three.html.twig');
     }
 }
--- src/Form/Customer/auth/SignUpType.php
+++ src/Form/Customer/auth/SignUpType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,10 +18,21 @@ class SignUpType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class, [
+                'label' => 'E-Mail',
+                'attr' => [
+                    'placeholder' => 'E-mail address',
+                    'class' => 'white-placeholder text-whiteblock p-2.5 pl-4 resize-none font-ubuntu text-white bg-gray-50 border-b-2 border-white rounded-t-md focus:ring-blue-500 focus:border-blue-500 mt-6 mb-2 bg-opacity-25',
+                ],
+            ])
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'label' => 'Password',
+                'attr' => [
+                    'placeholder' => 'Password',
+                    'class' => 'white-placeholder text-whiteblock p-2.5 pl-4 resize-none font-ubuntu text-white bg-gray-50 border-b-2 border-white rounded-t-md focus:ring-blue-500 focus:border-blue-500 mt-6 mb-2 bg-opacity-25',
+                    'autocomplete' => 'new-password',
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Choose your password',
