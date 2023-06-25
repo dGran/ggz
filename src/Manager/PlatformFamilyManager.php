f5d832b352c58eff1fc22fc0c69138030a762f30
dgran <dgranh@gmail.com>
2023-06-25 11:03:55 +0200

    [IMP] Add PlatformFamily entity, manager and repository, remove toReview version

---
+++ src/Manager/PlatformFamilyManager.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Manager;
+
+use App\Entity\PlatformFamily;
+use App\Repository\PlatformFamilyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class PlatformFamilyManager
+{
+    protected EntityManagerInterface $entityManager;
+    protected PlatformFamilyRepository $repository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->repository = $this->entityManager->getRepository(PlatformFamily::class);
+    }
+
+    public function create(): PlatformFamily
+    {
+        return new PlatformFamily();
+    }
+
+    public function save($platformFamily): PlatformFamily
+    {
+        $this->entityManager->persist($platformFamily);
+        $this->entityManager->flush();
+
+        return $platformFamily;
+    }
+
+    public function delete(PlatformFamily $platformFamily): PlatformFamily
+    {
+        $this->entityManager->remove($platformFamily);
+        $this->entityManager->flush();
+
+        return $platformFamily;
+    }
+
+    public function findOneById($id): ?PlatformFamily
+    {
+        /** @var PlatformFamily $platformFamily */
+        $platformFamily = $this->repository->find($id);
+
+        return $platformFamily;
+    }
+
+    public function findOneBy(array $criteria, array $orderBy = null): PlatformFamily
+    {
+        /** @var PlatformFamily $platformFamily */
+        $platformFamily = $this->repository->findOneBy($criteria, $orderBy);
+
+        return $platformFamily;
+    }
+
+    public function findAll(): array
+    {
+        return $this->repository->findAll();
+    }
+
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null): array
+    {
+        return $this->repository->findBy($criteria, $orderBy, $limit, $offset);
+    }
+
+    /**
+     * @param PlatformFamily[] $platformFamilies
+     *
+     * @return PlatformFamily[]
+     */
+    public function saveCollection(array $platformFamilies): array
+    {
+        foreach ($platformFamilies as $platformFamily) {
+            $this->entityManager->persist($platformFamily);
+        }
+
+        $this->entityManager->flush();
+
+        return $platformFamilies;
+    }
+}
---
+++ src/Repository/PlatformFamilyRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\PlatformFamily;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<PlatformFamily>
+ *
+ * @method PlatformFamily|null find($id, $lockMode = null, $lockVersion = null)
+ * @method PlatformFamily|null findOneBy(array $criteria, array $orderBy = null)
+ * @method PlatformFamily[]    findAll()
+ * @method PlatformFamily[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PlatformFamilyRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, PlatformFamily::class);
+    }
+}
