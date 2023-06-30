5f3805bc17bb73609166abc2472934a34b00243e
dgran <dgranh@gmail.com>
2023-06-30 18:02:07 +0200

    [IMP] Improved code

--- src/Entity/Universe.php
+++ src/Entity/Universe.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Repository\UniverseRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UniverseRepository::class)]
@@ -30,6 +32,14 @@ class Universe
     #[ORM\Column(nullable: true)]
     private ?int $universeId = null;
 
+    #[ORM\OneToMany(mappedBy: 'universe', targetEntity: Series::class)]
+    private Collection $series;
+
+    public function __construct()
+    {
+        $this->series = new ArrayCollection();
+    }
+
     public function getId(): int
     {
         return $this->id;
@@ -40,7 +50,7 @@ class Universe
         return $this->name;
     }
 
-    public function setName(string $name): static
+    public function setName(string $name): Universe
     {
         $this->name = $name;
 
@@ -52,7 +62,7 @@ class Universe
         return $this->description;
     }
 
-    public function setDescription(?string $description): static
+    public function setDescription(?string $description): Universe
     {
         $this->description = $description;
 
@@ -64,7 +74,7 @@ class Universe
         return $this->alternateName;
     }
 
-    public function setAlternateName(?string $alternateName): static
+    public function setAlternateName(?string $alternateName): Universe
     {
         $this->alternateName = $alternateName;
 
@@ -76,7 +86,7 @@ class Universe
         return $this->picture;
     }
 
-    public function setPicture(?string $picture): static
+    public function setPicture(?string $picture): Universe
     {
         $this->picture = $picture;
 
@@ -88,9 +98,39 @@ class Universe
         return $this->universeId;
     }
 
-    public function setUniverseId(?int $universeId): static
+    public function setUniverseId(?int $universeId): Universe
     {
         $this->universeId = $universeId;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Series>
+     */
+    public function getSeries(): Collection
+    {
+        return $this->series;
+    }
+
+    public function addSeries(Series $series): Universe
+    {
+        if (!$this->series->contains($series)) {
+            $this->series->add($series);
+            $series->setUniverse($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSeries(Series $series): Universe
+    {
+        if ($this->series->removeElement($series)) {
+            // set the owning side to null (unless already changed)
+            if ($series->getUniverse() === $this) {
+                $series->setUniverse(null);
+            }
+        }
 
         return $this;
     }
